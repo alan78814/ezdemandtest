cb00d4edd4ee0363e7841f26079c3fee898dd4b7
alan78814 <tri78814@gmail.com>
2023-06-08 20:28:46 +0800

    mac test

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import axios from 'axios';
 import dayjs from 'dayjs';
-import path from 'path'
+import path from 'path';
 
 const app = express();
 // For parsing application/json
 app.use(express.json());
- 
+
 // For parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
@@ -18,35 +18,37 @@ app.get('/:filename', (req, res) => {
   res.sendFile(filePath);
 });
 
-app.get('/test', async (req , res) => {
+app.get('/test', async (req, res) => {
   try {
     function drawChart(demandData: Record<string, number>, time: number) {
       const labels = Object.keys(demandData);
       const data = Object.values(demandData);
-      
+
       const chartData = {
         labels: labels,
-        datasets: [{
-          label: `Demand Data-${dayjs(time).format('YYYY-MM-DD')}`,
-          backgroundColor: 'rgba(255, 99, 132, 0.2)',
-          borderColor: 'rgba(255, 99, 132, 1)',
-          borderWidth: 1,
-          data: data,
-        }]
+        datasets: [
+          {
+            label: `Demand Data-${dayjs(time).format('YYYY-MM-DD')}`,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+            data: data,
+          },
+        ],
       };
-      
+
       const config = {
         type: 'bar',
         data: chartData,
         options: {
           scales: {
             y: {
-              beginAtZero: true
-            }
-          }
-        }
+              beginAtZero: true,
+            },
+          },
+        },
       };
-      
+
       const canvas = '<canvas id="myChart"></canvas>';
       const script = `
         <script>
@@ -54,7 +56,7 @@ app.get('/test', async (req , res) => {
           var myChart = new Chart(ctx, ${JSON.stringify(config)});
         </script>
       `;
-      
+
       return `
       <html>
         <head>
@@ -66,12 +68,12 @@ app.get('/test', async (req , res) => {
         </body>
       </html>`;
     }
-    
-    let { timeStamp, chartType, deviceName, apiName} = req.query;
 
-    const host = '192.168.0.201'
-    const port = 9999
-    const timeStampValue = dayjs(timeStamp as string).valueOf() // 1651622400000 5/4
+    let { timeStamp, chartType, deviceName, apiName } = req.query;
+
+    const host = '192.168.0.201';
+    const port = 9999;
+    const timeStampValue = dayjs(timeStamp as string).valueOf(); // 1651622400000 5/4
     // const chartType = 'day'
     // const deviceName = '(電錶)電盤總電'
 
@@ -79,17 +81,20 @@ app.get('/test', async (req , res) => {
       timeout: 5000, // 設定 5 秒的超時時間
     };
 
-    const result = await axios(`http://${host}:${port}/ezcon/api/${apiName}?timeStamp=${timeStampValue}&chartType=${chartType}&deviceName=${deviceName}`,axiosConfig)
+    const result = await axios(
+      `http://${host}:${port}/ezcon/api/${apiName}?timeStamp=${timeStampValue}&chartType=${chartType}&deviceName=${deviceName}`,
+      axiosConfig
+    );
     const demandData = result.data;
-    console.log('demandData',demandData)
+    console.log('demandData', demandData);
 
-    const chartHtml = drawChart(demandData,timeStampValue);
-    
+    const chartHtml = drawChart(demandData, timeStampValue);
+
     res.setHeader('Content-Type', 'text/html');
     res.write(chartHtml);
     res.end();
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.status(500).send(`
     <html>
       <head>
@@ -101,21 +106,20 @@ app.get('/test', async (req , res) => {
       </body>
     </html>
   `);
-
   }
-})
+});
 
-app.post('/tonnetTest',(req, res) => {
-  console.log('接受通航外拋事件')
+app.post('/tonnetTest', (req, res) => {
+  console.log('接受通航外拋事件');
   if (dayjs(req.body.time).isAfter('2023-05-12 13:35')) {
-    console.log('reqbody',req.body)
-    console.log('===============================')
-  } 
+    console.log('reqbody', req.body);
+    console.log('===============================');
+  }
 
   // if (req.body.dev_name === '門口機112' && dayjs(req.body.time).isAfter('2023-05-12 13:35')) {
   //   console.log('reqbody',req.body)
   //   console.log('===============================')
-  // } 
+  // }
 
   // console.log('reqbody',req.body)
   // console.log('===============================')
@@ -140,16 +144,16 @@ app.get('/', async (req, res) => {
     </style>
 
     <form action="/test" method="GET">
-      <label for="timeStamp">日期:</label>
+      <label for="timeStamp">日期mactest:</label>
       <input type="text" id="date" name="timeStamp"><br><br>
       
-      <label for="chartType">圖表類型:</label>
+      <label for="chartType">圖表類型mactest:</label>
       <input type="text" id="chartType" name="chartType"><br><br>
       
-      <label for="deviceName">儀器名稱:</label>
+      <label for="deviceName">儀器名稱mactest:</label>
       <input type="text" id="deviceName" name="deviceName"><br><br>
 
-      <label for="apiName">api名稱:</label>
+      <label for="apiName">api名稱mactest:</label>
       <input type="text" id="apiName" name="apiName"><br><br>
       
       <button type="submit">送出</button>
